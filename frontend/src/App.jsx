d12e19bd016065337810b48b55e43e02c09b3219
Shivani Reddy <shivanireddy313@gmail.com>
2026-05-16 10:50:41 +0530

    Connect donate/requests buttons to forms

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import LoginPage from './Pages/LoginPage/LoginPage'
 import Home from './Pages/Home/Home'
 import RegisterPage from './components/register/Register'
 import RoleSelectPage from './components/role/RoleSelect'
+import DonorForm from './Components/Forms/DonarForm'
+import ReceiverForm from './Components/Forms/Receiver'
 
 export default function App() {
   return (
@@ -14,7 +16,12 @@ export default function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/select-role" element={<RoleSelectPage />} />
+
+        {/* Donate / Receive */}
+        <Route path="/donate" element={<DonorForm />} />
+        <Route path="/requests" element={<ReceiverForm />} />
       </Routes>
     </BrowserRouter>
   )
 }
+
